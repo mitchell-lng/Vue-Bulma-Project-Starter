5fa718f8b2bfa1551df51dd7d61112328e6ecfe5
Mitchell Long <mitchelltlong@gmail.com>
2021-04-08 08:55:47 -0700

    Added _redirects page to template.

--- startproject.py
+++ startproject.py
@@ -126,6 +126,10 @@ $input-shadow: none;
 @import '../../node_modules/bulma/sass/layout/hero.sass';
 @import '../../node_modules/bulma/sass/layout/section.sass';
 @import '../../node_modules/bulma/sass/helpers/_all.sass';
+@import '../../node_modules/bulma/sass/grid/columns.sass';
+@import '../../node_modules/bulma/sass/elements/box.sass';
+@import '../../node_modules/bulma/sass/elements/image.sass';
+@import '../../node_modules/bulma/sass/elements/content.sass';
 """)
 
 # Move into the components folder to update the home page
@@ -207,6 +211,12 @@ export default {
 
 <style scoped>
 </style>""")
+	
+os.chdir('../../public')
+
+# Add the redirects page for many hosting services becuase SPA
+with open('_redirects', 'w') as file:
+	file.write("/*    /index.html   200")
 
 # Tell user that the script finished
 os.system('cls')
